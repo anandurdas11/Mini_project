1617dc816a90d236a7a216c71127403b232281b3
anandurdas11 <anandurdas11@gmail.com>
2022-05-30 15:14:22 -0700

    top bar created and icon added

--- bloggin_webapp/src/App.js
+++ bloggin_webapp/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import { AppBar, Toolbar } from '@material-ui/core';
+
 function App() {
   return (
-    <div className="App">
-     Blogging webapp
+    <div>
+      <body>
+        <AppBar className="Appbar" position="static">
+          <Toolbar>
+            <header>
+              <img
+                src="https://cdn0.iconfinder.com/data/icons/font-awesome-solid-vol-1/512/blog-128.png"
+                className="topAvatar" alt="icon"
+              />
+              
+            </header>
+          </Toolbar>
+        </AppBar>
+      </body>
     </div>
   );
 }
