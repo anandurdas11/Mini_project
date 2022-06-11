b6dedd2344c16a773aa103000119096801a68b99
anandurdas11 <anandurdas11@gmail.com>
2022-06-11 10:54:25 -0700

    top bar changed

--- bloggin_webapp/src/Components/Topbar.jsx
+++ bloggin_webapp/src/Components/Topbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import "./topbar.css"
+import { AppBar, Toolbar } from '@material-ui/core';
 
 export default function topbar() {
   return (
     <div className="topbar">
-      <div className="topbarwrapper">
-        <div className="topleft">
-          top left 
+      <AppBar>
+       
+        <div className="topbarwrapper">
+          <Toolbar>
+            <div className="topleft">
+              top left
+            </div>
+            
+          </Toolbar>
+          <Toolbar>
+            <div className="topright">
+              top right
+            </div>
+          </Toolbar>
+          
+
+          
         </div>
-        <div className="topright">
-          top right
-        </div>
-      </div>
+
+      </AppBar>
+    
     </div>
   )
 }
