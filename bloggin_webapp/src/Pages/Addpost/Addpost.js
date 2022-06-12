14020fb96fc2ea92168226e673b4733556f8dddf
anandurdas11 <anandurdas11@gmail.com>
2022-06-12 09:40:22 -0700

    some compnents added

--- bloggin_webapp/src/Pages/Addpost/Addpost.js
+++ bloggin_webapp/src/Pages/Addpost/Addpost.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Add } from "@material-ui/icons";
+import { TextField ,Select ,InputLabel,FormControl,MenuItem } from "@material-ui/core";
 function Addpost() {
   return (
-      <div>
-        <Add /> 
+    <div>
+      <form>
+        <TextField fullwidth variant="outlined" label="title"></TextField>
+        <br></br>
+        <br></br>
+        <TextField variant="outlined" label="articlebody"></TextField>
+        <br></br>
+        <br></br>
+        <FormControl fullWidth>
+          {/*<InputLabel id="simple">Catgoery</InputLabel>*/}
+        </FormControl>
+        <Select labelId="simple" id="simple">
+          
+          <MenuItem value={"TECH"}>Technology</MenuItem>
+          <MenuItem value={"TECH"}>Art</MenuItem>
+         
+        </Select>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Addpost
