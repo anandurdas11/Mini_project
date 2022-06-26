5042f1c172b42b2b26b78a116ded7acbb9fb6302
anandurdas11 <anandurdas11@gmail.com>
2022-06-25 20:51:09 -0700

    delete and edit page added

--- bloggin_webapp/src/Pages/Home/More.js
+++ bloggin_webapp/src/Pages/Home/More.js
@@ -2,6 +2,7 @@ import { IconButton, Menu, MenuItem } from "@material-ui/core";
 import { MoreVertRounded } from "@material-ui/icons";
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 function Test() {
   const isAuth = localStorage.getItem("isAuth");
   const [anchorEl, setanchorEl] = useState(null);
@@ -16,29 +17,40 @@ function Test() {
    };
   return (
     <div>
-       {!isAuth ? (
-       <IconButton onClick={handleunauth}>
-        <MoreVertRounded style={{ color: "black" ,marginRight:"3%" }} />
-      </IconButton>
+      {!isAuth ? (
+        <IconButton onClick={handleunauth}>
+          <MoreVertRounded style={{ color: "black", marginRight: "3%" }} />
+        </IconButton>
       ) : (
         <>
           <IconButton onClick={openMenu}>
-        <MoreVertRounded style={{ color: "black" ,marginRight:"auto",display:'flex' }} />
-      </IconButton>
+            <MoreVertRounded
+              style={{ color: "black", marginRight: "auto", display: "flex" }}
+            />
+          </IconButton>
           <div className="menu">
-          <Menu
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={onclose}
-      >
-        <MenuItem onClick={onclose}>Edit</MenuItem>
-        <MenuItem onClick={onclose}>delete</MenuItem>
-      </Menu>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={onclose}
+            >
+              <Link
+                to="/Edit"
+                style={{ color: "black", textDecoration: "none" }}
+              >
+                <MenuItem onClick={onclose}>Edit</MenuItem>
+              </Link>
+              <Link
+                to="Delete"
+                style={{ color: "black", textDecoration: "none" }}
+              >
+                <MenuItem onClick={onclose}>delete</MenuItem>
+              </Link>
+            </Menu>
           </div>
         </>
       )}
-      
     </div>
   );
 }
---
+++ bloggin_webapp/src/Pages/Modify/Edit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Edit() {
+    console.log("hello");
+  return (
+      <div style={{ marginTop: "40%" }}></div>
+      
+  )
+}
+
+export default Edit
