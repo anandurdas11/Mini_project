c3599e61c18b625f37e09e6fb3a78b4dae24492d
anandurdas11 <anandurdas11@gmail.com>
2022-09-19 18:31:33 -0700

    some database changes

--- bloggin_webapp/src/firebase.js
+++ bloggin_webapp/src/firebase.js
@@ -2,16 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbCgs7A3qUCqz5Q0qoKCYFXv8tz7xCqwY",
-  authDomain: "blogging-webversion2-52351.firebaseapp.com",
-  projectId: "blogging-webversion2-52351",
-  storageBucket: "blogging-webversion2-52351.appspot.com",
-  messagingSenderId: "668249554167",
-  appId: "1:668249554167:web:0d6eb4bc9637d7d9e927f2"
+  apiKey: "AIzaSyCJWk8ZQfegv9L-cFeFGVrcgI1uVMsQ39w",
+  authDomain: "blogging-webv2.firebaseapp.com",
+  projectId: "blogging-webv2",
+  storageBucket: "blogging-webv2.appspot.com",
+  messagingSenderId: "840182398620",
+  appId: "1:840182398620:web:cf7c1a2e84e6ca642cf759"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
